f8a2df3e986295afa8e504154fd939006b0843a7
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 18:43:21 +0530

    Update variables.tf

--- Terraform/main/nonprod/variables.tf
+++ Terraform/main/nonprod/variables.tf
@@ -17,3 +17,26 @@ variable "location"{
 variable "tags"{
   type = map
 }
+
+variable "vnet_address_space" {
+  type = list
+}
+
+variable "subnetaddress" {
+  type = list
+  default = ["10.0.1.0/24"]
+}
+
+variable "Serviceendpoints"{
+ type = list
+}
+variable "private_endpoint_network_polciies_enables"{
+  description = "private end point link polciy "
+}
+
+variable "allow_ips"{
+ type = list
+}
+
+
+
