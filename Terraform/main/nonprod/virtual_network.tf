78f07d523bcfbfa76f8c4254192a1a95a3240d74
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 23:48:18 +0530

    Update virtual_network.tf

--- Terraform/main/nonprod/virtual_network.tf
+++ Terraform/main/nonprod/virtual_network.tf
@@ -1,6 +1,6 @@
 module virtual_network {
   source                  = "../../modules/azure_virtual_network"
-  name                    = "vn-${var.resource_name}-${var.resource_namespace}
+  name                    = "vn-${var.resource_name}-${var.resource_namespace}"
   resource_group_name     = module.resource_group.name
   address_space           = var.vnet_Address_space
   location                = module.resoorce_group.location
