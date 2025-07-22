355bd17ba2814008593631bf9188caf1f5ffeaef
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 16:30:25 +0530

    Create variables.tf

---
+++ Terraform/main/nonprod/variables.tf
@@ -0,0 +1,19 @@
+variable "environment_name"
+{
+  type = string
+}
+
+variable "resource_name"
+{
+  type = string
+}
+
+variable "resource_namepsace"{
+  type = string
+}
+variable "location"{
+  type = string
+}
+variable "tags"{
+  type = map
+}
