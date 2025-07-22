ec70cf1eeb46112cf4afaa79b3dc1aab0dc384a4
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 19:50:32 +0530

    Create outputs.tf

---
+++ Terraform/modules/azure_virtual_network/outputs.tf
@@ -0,0 +1,15 @@
+output "id" {
+ value = "${azurerm_virtual_network.virtual_network.id}"
+}
+
+output "name" {
+ value = "${azurerm_virtual_network.virtual_network.name}"
+}
+
+output "resource_group_name" {
+  value = "${azurerm_virtual_network.virtual_network.resource_group_name}"
+}
+
+output "location" {
+  value = "${azurerm_virtual_network.virtual_network.location}"
+}
