1e051b9ef43f0c13e382b23499142e2cc181c1f4
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 18:13:29 +0530

    Create main.tf

---
+++ Terraform/modules/azure_subnet/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_subnet" "example" {
+  name                 = var.name
+  resource_group_name  = var.resource_group_name
+  service_endpoints = var.service_endpoints
+  address_prefixes     = var.address_prefixes
+  private_endpoint_network_policies_enabled = var.private_endpoint_network_policies_enabled
+
+  dynamic "delegation" {
+    for_each = ar.subnet_delegation
+    content {
+      name = delegation.key
+      dynamic "service_delegation" {
+        for_each = toset(delegation.value)
+        content{
+          name = service_delegation.value.name
+          actions = service_delegation.value.actions
+          }
+        }
+      }
+     }
+    }
+   resource "azurerm_subnet_network_Security_group_association" "subnet"{
+     subnet_id = azurerm_subnet.subnet.id
+     network_security_group_id = var.network_security_group_id
+     }
+     
+          
+        
+
+    
