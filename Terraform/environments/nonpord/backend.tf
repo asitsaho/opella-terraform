a56843d338467f565e74d997c7704840b032d95e
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 23:09:34 +0530

    Update backend.tf

--- Terraform/environments/nonpord/backend.tf
+++ Terraform/environments/nonpord/backend.tf
@@ -1,8 +1,8 @@
 resource_group_name = "rg-opella-infra-tf-backend"
-storage_account_name = "saopinfratfbackend"
+storage_account_name = "saopellainfratfbackend"
 container_name = "tfstate"
 key = "dev.opella.tfstate"
 use_oidc = true
 use_azuread_auth = true
-subscription_id= ""
-tenant_id=""
+subscription_id= "56fe70fb-4d0c-4a30-beaa-5059b10acdf3"
+tenant_id="64224299-dfb4-4d7c-b608-b1ce2dbf1122"
