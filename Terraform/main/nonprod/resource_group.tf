2554c1e3fab91a27e92e248d69788efe6a50af67
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 23:47:27 +0530

    Update resource_group.tf

--- Terraform/main/nonprod/resource_group.tf
+++ Terraform/main/nonprod/resource_group.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
    source                  ="../../modules/azure_resource_group"
-   name                    = "rg-${var.resource_name}-${var.resource_namespace}
+   name                    = "rg-${var.resource_name}-${var.resource_namespace}"
    #defaults to centralus
    location                = var.location
    tags                    = var.tgs
