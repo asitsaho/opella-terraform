81c8c204f0dc2d9f9f473a2125020396dd4c030f
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-22 16:06:22 +0530

    Create backend.tf

---
+++ Terraform/environments/nonpord/backend.tf
@@ -0,0 +1,8 @@
+resource_group_name = "rg-opella-infra-tf-backend"
+storage_account_name = "saopinfratfbackend"
+container_name = "tfstate"
+key = "dev.opella.tfstate"
+use_oidc = true
+use_azuread_auth = true
+subscription_id= ""
+tenant_id=""
