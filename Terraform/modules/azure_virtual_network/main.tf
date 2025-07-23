b3898cacb1ba6f6e428ba116465b84b140686ad0
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-23 11:21:25 +0530

    Update main.tf

--- Terraform/modules/azure_virtual_network/main.tf
+++ Terraform/modules/azure_virtual_network/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "virtual_network" {
   name                  = var.name
-  address_Space         = var.address_Space
+  address_space         = var.address_space
   location              = var.location
   resource_group_name   = var.resource_group_name
   tags                  = var.tags
