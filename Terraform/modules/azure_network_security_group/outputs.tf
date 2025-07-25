9a9bac78a855f2f618573eab9e95b4a8e76b337a
asitsaho <36837304+asitsaho@users.noreply.github.com>
2025-07-25 11:02:54 +0530

    Update outputs.tf

--- Terraform/modules/azure_network_security_group/outputs.tf
+++ Terraform/modules/azure_network_security_group/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
  value  =  "${azurerm_network_security_group.network_security_group.id}"
- description = "The network securoty Group ID."
+ description = "The network security Group ID."
 }
 
 output "name" {
   value = "${azurerm_network_security_group.network_security_group.name}"
-  description = "The network securoty Group name "
+  description = "The network security Group name "
 }
 
 output "location" {
   value = "${azurerm_network_security_group.network_security_group.location}"
-  description = "The network securoty Group location"
+  description = "The network security Group location"
 }
